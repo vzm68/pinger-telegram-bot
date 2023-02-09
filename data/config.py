31e5569968d13efb6184da8b9fbd735ccbd0b51b
Vitalii <120312418+vzm68@users.noreply.github.com>
2023-02-09 23:48:31 +0300

    Update config.py
    Comments update

--- data/config.py
+++ data/config.py
@@ -11,5 +11,5 @@ TOKEN = env.str("TOKEN")  # TelegramBot token from @BotFather - str
 chat_id = env.int("chat_id")  # Admin chat ID - int
 group_id = env.int("group_id")  # Group chat ID, not necessary but in my case it was needed - int
 ip = env.str("IP")  # IP address. In future case should be list type - str
-login = env.str("login")
-log_path = env.str("log_path")
+login = env.str("login")  # Telegram login for reminder and push notification - str
+log_path = env.str("log_path")  # Path to .log file - str
