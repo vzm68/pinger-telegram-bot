3ff57c3349140f749357762e0e41caf50e4c61ba
Vitalii <120312418+vzm68@users.noreply.github.com>
2023-02-09 23:55:14 +0300

    Update tail_log.py
    Update comments

--- exe/tail_log.py
+++ exe/tail_log.py
@@ -2,5 +2,9 @@ from data.config import log_path
 
 
 def tail_log():
+    """
+    I need this func to get last ten
+    logs strings.
+    """
     with open(log_path, 'r') as file:
         return file.readlines()[-10:]
