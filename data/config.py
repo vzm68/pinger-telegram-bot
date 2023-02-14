cb30e57b71d5473f957836fac657e3273c9b610d
Vitalii <zabrodskyi.vitalii@gmail.com>
2023-02-14 14:34:35 +0200

    Update general code to ping several IP 1.0

--- data/config.py
+++ data/config.py
@@ -1,5 +1,10 @@
 from environs import Env
 
+ip_list = {
+    '10.100.3.228': [True, 0],
+    '127.0.0.2': [True, 0]
+}
+
 """
 Get personal data from .env
 """
@@ -10,6 +15,5 @@ env.read_env()
 TOKEN = env.str("TOKEN")  # TelegramBot token from @BotFather - str
 chat_id = env.int("chat_id")  # Admin chat ID - int
 group_id = env.int("group_id")  # Group chat ID, not necessary but in my case it was needed - int
-ip = env.str("IP")  # IP address. In future case should be list type - str
 login = env.str("login")  # Telegram login for reminder and push notification - str
 log_path = env.str("log_path")  # Path to .log file - str
